7246a20df4f6c7b95783121a860fad061749dc9c
Shono Ogawa <shono@Shonos-MacBook-Pro.local>
2024-07-15 22:45:38 +0900

    Implement survey results display in SurveyResults component

--- survey-frontend/src/components/SurveyResults.js
+++ survey-frontend/src/components/SurveyResults.js
@@ -8,4 +8,31 @@ function SurveyResults() {
 
   useEffect(() => {
     const fetchSurvey = async () => {
-      const res = await axios.get(`https://survey-system-jhjx.onrender.com/api/surveys/${id}')
+      const res = await axios.get(`https://survey-system-jhjx.onrender.com/api/surveys/${id}`);
+      setSurvey(res.data);
+    };
+    fetchSurvey();
+  }, [id]);
+
+  if (!survey) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{survey.title}</h1>
+      {survey.questions.map((question, index) => (
+        <div key={index}>
+          <h3>質問 {index + 1}: {question.text}</h3>
+          <ul>
+            {question.options.map((option, optionIndex) => (
+              <li key={optionIndex}>
+                {option.text}: {option.count} 票
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SurveyResults;
